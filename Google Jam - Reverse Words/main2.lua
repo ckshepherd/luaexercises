4b1a106111f4ee6684b10a61f526121214d2d1e7
sclark39 <sclark@freolic.com>
2014-06-28 16:52:45 -0700

    Add lots of comments. Little bit of code cleanup.

--- Google Jam - Reverse Words/main2.lua
+++ Google Jam - Reverse Words/main2.lua
@@ -1,34 +1,37 @@
-googleString = "this is a test foobar all your base class pony along"
-
 function ReverseWords(s)
 	-- Tokenize
 	local words = {}
 	for a in s:gmatch("[^%s]+") do
-			table.insert(words,a)
+		words[#words+1] = a
 	end
 	
+	-- Create a reversed version of the 'words' table
 	local reversed = {}
 	for i = 1,#words do
 		reversed[i] = words[#words-i+1]
 	end
 	
+	-- Concat reversed version with spaces and return string
 	return table.concat(reversed," ")
 end
 
-local reversedString = ReverseWords( googleString )
-
-print( reversedString )
-
+-- Opens a file for read and another for write using the same name
 local file = "input"
 io.input( file..".in" )
 io.output( file..".out" )
 
+-- Reads the number of cases
 local numCases = tonumber( io.read() ) or 0
 print( "NumCases: "..numCases )
 
+-- Reads data for each case
 for case=1,numCases do
 	local line = io.read()
+	
+	-- Get and format answer
 	local answer = string.format( "Case #%d: %s", case, ReverseWords( line ) )
 	print( answer )
+	
+	-- Write answer to output file
 	io.write( answer.."\n" )
 end
--- Google Jam - Store Credit/main2.lua
+++ Google Jam - Store Credit/main2.lua
@@ -1,12 +1,5 @@
-local file = "input"
-io.input( file..".in" )
-io.output( file..".out" )
-
-local firstLine = io.read()
-local numCases = tonumber( firstLine )
-
-print ( "Num Cases: " .. numCases ) 
-
+-- Shepherd's algorithm. Test each item to see if it any item after it 
+-- add up to 'credit'. Return both item indices when answer is found.
 function FindBestStoreCredit( credit, itemprices )
 	for i=(1),(#itemprices-1) do
 		for j=(i+1),(#itemprices) do
@@ -17,12 +10,24 @@ function FindBestStoreCredit( credit, itemprices )
 	end
 end
 
+
+-- Opens a file for read and another for write using the same name
+local file = "input"
+io.input( file..".in" )
+io.output( file..".out" )
+
+-- Reads the number of cases
+local numCases = tonumber( io.read() )
+print ( "Num Cases: " .. numCases ) 
+
 for case=1,numCases do
-	
+		
+	-- Reads data for each case
 	local credit = tonumber( io.read() )
 	local numitems = tonumber( io.read() )
 	local itemprices = {}
 	
+	-- Tokenize and converting to number type
 	local itemsLine = io.read()
 	for itemPrice in string.gmatch( itemsLine, "[^%s]+" ) do
 		itemprices[#itemprices+1] = tonumber( itemPrice )
@@ -32,10 +37,13 @@ for case=1,numCases do
 	--print( "Num Items: " .. numitems )
 	--print( "Item prices: " .. table.concat( itemprices, ", " ) )
 	
+	-- Get answer
 	local item1,item2 = FindBestStoreCredit( credit, itemprices )
 	
+	-- Format answer
 	local answer = string.format( "Case #%d: %d %d", case, item1, item2 )
 	print( answer )
 	
+	-- Write answer to output file
 	io.write( answer.."\n" )
 end
